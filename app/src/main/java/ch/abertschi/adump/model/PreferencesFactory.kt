bebd5144b927d9643ef65be3be4ecdc818eb6048
Andrin <andrin.bertschi@gmail.com>
2017-04-24 16:40:14 +0200

    add header

--- app/src/main/java/ch/abertschi/adump/model/PreferencesFactory.kt
+++ app/src/main/java/ch/abertschi/adump/model/PreferencesFactory.kt
@@ -1,3 +1,9 @@
+/*
+ * Ad Free
+ * Copyright (c) 2017 by abertschi, www.abertschi.ch
+ * See the file "LICENSE" for the full license governing this code.
+ */
+
 package ch.abertschi.adump.model
 
 import android.content.Context
