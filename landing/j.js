871cbffb86db2861c218df58b461a4b5b75b0cb9
Andrin Bertschi <abertschi@abertschi.ch>
2022-08-15 11:40:15 +0200

    landing

---
+++ landing/j.js
@@ -0,0 +1,12 @@
+(function() {
+    cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+      $('header').addClass('rf');
+      $('h1').addClass('sw');
+      $('h2').addClass('sw');
+      $('img').addClass('sw');
+      $('a').addClass('sw');
+      $('div').addClass('sw');
+      $('button').addClass('sw');
+      $('li').addClass('sw');
+    });
+})();
