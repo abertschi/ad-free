e4789fb4f951790f3be41b9dbf4339681c440eb5
Andrin Bertschi <abertschi@abertschi.ch>
2022-08-17 14:49:59 +0200

    tracing

---
+++ app/src/main/java/ch/abertschi/adfree/view/mod/TracePresenter.kt
@@ -0,0 +1,34 @@
+package ch.abertschi.adfree.view.mod
+
+import android.content.Intent
+import ch.abertschi.adfree.AdFreeApplication
+import ch.abertschi.adfree.detector.*
+import org.jetbrains.anko.AnkoLogger
+
+class TracePresenter(val view: ActiveDetectorActivity) : AnkoLogger {
+
+    private val detectorFactory = (view.applicationContext as AdFreeApplication).adDetectors
+    private val prefs = (view.applicationContext as AdFreeApplication).prefs
+
+    fun getDetectors(category: String) = detectorFactory.getDetectorsForCategory(category)
+
+    fun isEnabled(d: AdDetectable) = detectorFactory.isEnabled(d)
+
+    fun onDetectorToggled(enable: Boolean, detector: AdDetectable) {
+        detectorFactory.setEnable(enable, detector)
+        detectorFactory.persistMeta()
+        showAdditionalInfoFor(detector, enable)
+    }
+
+    fun showAdditionalInfoFor(d: AdDetectable, enable: Boolean) {
+        if (d is AbstractDebugTracer && enable) {
+            view.showInfo("recording to " + (d.storageFolder?.absolutePath ?: "not available, check permissions"))
+        }
+
+        if (d is UserDefinedTextDetector && enable) {
+            // launch activity
+            val myIntent = Intent(this.view, GenericTextDetectorActivity::class.java)
+            this.view.startActivity(myIntent)
+        }
+    }
+}
